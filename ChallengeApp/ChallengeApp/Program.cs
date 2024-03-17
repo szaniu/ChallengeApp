effc2c593eb429d3e5e9a3f96b3f97b61eb92b63
szaniu <szaniu@poczta.fm>
2024-03-17 19:49:12 +0100

    dzień 12

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -25,23 +25,3 @@ Console.WriteLine($"AVG: {statistics.Average}");
 Console.WriteLine($"Min: {statistics.Min}");
 Console.WriteLine($"Max: {statistics.Max}");
 Console.WriteLine($"Average Letter: {statistics.AverageLetter}");
-
-
-
-/*var employee = new Employee("Darek", "Szaniu");
-employee.AddGrade("200");
-employee.AddGrade(2.58);
-employee.AddGrade(5.51f);
-employee.AddGrade(99L);
-employee.AddGrade(1);
-employee.AddGrade(1);
-employee.AddGrade(-1);
-employee.AddGrade('A');
-var statistics = employee.GetStatistics();*/
-
-
-
-/*Console.WriteLine($"Average: {statistics.Average:N2}");
-Console.WriteLine($"Min: {statistics.Min}");
-Console.WriteLine($"Max: {statistics.Max}");
-Console.WriteLine($"Ocena literowa: {statistics.AverageLetter}");*/
